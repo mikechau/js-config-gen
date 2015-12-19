c1bd6ae072218b7b7912d57f86c1e96f3b6ad77c
Mike Chau <mike@chau.io>
2015-12-19 15:57:54 -0600

    Compile airbnb-eslint.js

--- scripts/compile-airbnb-eslint.js
+++ scripts/compile-airbnb-eslint.js
@@ -4,9 +4,17 @@
 var shell = require('shelljs');
 var path = require('path');
 var config = require('../src/configs/eslintrc/airbnb');
+var fs = require('fs');
+var tmpl = require('blueimp-tmpl').tmpl;
 
-var template = config.template;
-var directory = path.resolve(__dirname, '../dist/airbnb-eslintrc.json');
+var baseDir = path.resolve(__dirname, '../dist');
 
 shell.echo('----> Generating airbnb-eslintrc.json to dist...');
-template.to(directory);
+var jsonPath = path.join(baseDir, 'airbnb-eslintrc.json');
+config.template.to(jsonPath);
+
+shell.echo('----> Generating airbnb-eslintrc.js to dist...');
+var exportModuleTemplate = fs.readFileSync(path.resolve(__dirname, '../src/templates/module.js.tmpl'), 'utf8');
+var airbnbModule = tmpl(exportModuleTemplate, config.json);
+var modulePath = path.join(baseDir, 'airbnb-eslintrc.js');
+airbnbModule.to(modulePath);
