9124b7b4c39d805dbe4b5d11698229a1c402147b
Mike Chau <mike@chau.io>
2015-12-19 14:37:41 -0600

    Create entry point for tests

---
+++ tests/index.js
@@ -0,0 +1,9 @@
+var glob = require('glob');
+var path = require('path');
+
+var baseDir = path.resolve(__dirname);
+var testsDir = path.join(baseDir, '**', '*.test.js');
+
+glob.sync(testsDir).forEach(function(file) {
+  require(file);
+});
